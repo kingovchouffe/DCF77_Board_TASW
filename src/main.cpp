439de789ff32d2e6733ea3f9c3e132549b4bf8a0
kingovchouffe <46623009+kingovchouffe@users.noreply.github.com>
2023-06-04 12:43:56 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 #include <Arduino.h>
 
-// put function declarations here:
 
+#define BLINKPIN PB12
+#define DCF77PIN PB3
 
+int prevSensorValue=0;
+  
 void setup() {
-  // put your setup code here, to run once:
- 
+  Serial.begin(9600);
+  pinMode(DCF77PIN, INPUT);
+  pinMode(13, OUTPUT);
+  Serial.println("0ms       100ms     200ms     300ms     400ms     500ms     600ms     700ms     800ms     900ms     1000ms    1100ms    1200ms");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int sensorValue = digitalRead(DCF77PIN);
+  if (sensorValue==1 && prevSensorValue==0) { Serial.println("");   }
+  digitalWrite(BLINKPIN, sensorValue);
+  Serial.print(sensorValue);   
+  prevSensorValue = sensorValue;
+  delay(10);
 }
 
