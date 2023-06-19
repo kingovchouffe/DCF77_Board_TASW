765aac7b881d01e0ae97711e794aa4d4e7b3973f
kingovchouffe <46623009+kingovchouffe@users.noreply.github.com>
2023-06-19 18:55:11 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,64 @@
-#include <Arduino.h>
+#include <DCF77.h>
 
 
-#define BLINKPIN PB12
-#define DCF77PIN PB3
+#define DCF_INTERRUPT PB0
+#define DCF77PIN PB0
 
-int prevSensorValue=0;
-  
-void setup() {
-  Serial.begin(9600);
-  pinMode(DCF77PIN, INPUT);
-  pinMode(13, OUTPUT);
-  Serial.println("0ms       100ms     200ms     300ms     400ms     500ms     600ms     700ms     800ms     900ms     1000ms    1100ms    1200ms");
+time_t prevDisplay = 0;          // when the digital clock was displayed
+time_t time;
+DCF77 DCF = DCF77(DCF77PIN,DCF_INTERRUPT);
+
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");*--
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
 }
 
-void loop() {
-  int sensorValue = digitalRead(DCF77PIN);
-  if (sensorValue==1 && prevSensorValue==0) { Serial.println("");   }
-  digitalWrite(BLINKPIN, sensorValue);
-  Serial.print(sensorValue);   
-  prevSensorValue = sensorValue;
-  delay(10);
+void digitalClockDisplay() {
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
+}
+
+unsigned long getDCFTime()
+{ 
+  time_t DCFtime = DCF.getTime();
+  // Indicator that a time check is done
+  if (DCFtime!=0) {
+    Serial.print("X");  
+  }
+  return DCFtime;
+}
+
+void setup() {
+  Serial.begin(115200); 
+  DCF.Start();
+  setSyncInterval(30);
+  setSyncProvider((getExternalTime)getDCFTime);
+  // It is also possible to directly use DCF.getTime, but this function gives a bit of feedback
+  //setSyncProvider(DCF.getTime);
+
+  Serial.println("Waiting for DCF77 time ... ");
+  Serial.println("It will take at least 2 minutes until a first update can be processed.");
+  
+}
+
+void loop()
+{  
+  if( now() != prevDisplay) //update the display only if the time has changed
+  {
+    prevDisplay = now();
+    digitalClockDisplay(); 
+  }
 }
 
